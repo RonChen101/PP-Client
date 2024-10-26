c12342fa9b9a1271140da2c5dbffb25dd4e93897
zdz <1918533826@qq.com>
2024-10-26 17:21:16 +0800

    删除test.java

--- src/test.java
+++
@@ -1,9 +0,0 @@
-
-import log.Manager;
-
-public class test {
-    public static void main(String[] args) throws Exception {
-        Manager manager = new Manager();
-        manager.read("log.json");
-        manager.extract();
-        System.out.println(manager.json.toString(4));
